e2586e392377fbd255fc9336e2e2bc774cd03efa
amiericanderson <ganderson5@eagles.ewu.edu>
2020-06-11 20:35:25 -0700

    Added test classes for DungeonCharacter, Hero, Monster, and Room to the tests folder

---
+++ tests/DungeonCharacterTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DungeonCharacterTest {
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getHitPoints() {
+    }
+
+    @Test
+    void getAttackSpeed() {
+    }
+
+    @Test
+    void addHitPoints() {
+    }
+
+    @Test
+    void subtractHitPoints() {
+    }
+
+    @Test
+    void isAlive() {
+    }
+
+    @Test
+    void attack() {
+    }
+}
